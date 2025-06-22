64a862013f8d40146bbbfbcd42b2117947d0258c
VuHuu <162543040+Vucore@users.noreply.github.com>
2025-06-22 21:41:00 +0700

    update crystal skill explode

--- Assets/Scripts/Skill/SkillManager.cs
+++ Assets/Scripts/Skill/SkillManager.cs
@@ -9,12 +9,13 @@ public class SkillManager : MonoBehaviour
     public Clone_Skill clone_Skill { get; private set; }
     public Sword_Skill sword_Skill { get; private set; }
     public Blackhole_Skill blackhole_Skill { get; private set; }
-    private void Awake() 
+    public Crystal_Skill crystal_Skill { get; private set; }
+    private void Awake()
     {
-        if(instance != null)
+        if (instance != null)
             Destroy(instance.gameObject);
-        else 
-            instance = this;    
+        else
+            instance = this;
     }
     private void Start()
     {
@@ -22,5 +23,6 @@ public class SkillManager : MonoBehaviour
         clone_Skill = GetComponent<Clone_Skill>();
         sword_Skill = GetComponent<Sword_Skill>();
         blackhole_Skill = GetComponent<Blackhole_Skill>();
+        crystal_Skill = GetComponent<Crystal_Skill>();
     }
 }
